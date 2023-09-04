487ccd35b03bc2f72a5909ee4b1e3b4217673eeb
shan <shan0815332@gmail.com>
2023-09-04 16:34:38 +0800

    Push Hw1 & Hw2 to GitHub

---
+++ src/hw/Hw1.java
@@ -0,0 +1,65 @@
+package hw;
+
+public class Hw1 {
+	public static void main(String[] args) {
+
+		// 請設計一隻Java程式,計算12,6這兩個數值的和與積
+		int num1 = 12, num2 = 6;
+
+		System.out.println("第一題");
+		System.out.println("12+6=" + (num1 + num2));
+		System.out.println("12*6=" + (num1 * num2));
+
+		
+		// 請設計一隻Java程式,計算200顆蛋共是幾打幾顆? (一打為12顆)
+		int eggAmount = 200;
+		int eggDozen = eggAmount / 12;
+		int eggExtra = eggAmount % 12;
+
+		System.out.println("\n第二題");
+		System.out.println(eggAmount + "顆蛋共是" + eggDozen + "打" + eggExtra + "顆");
+
+		
+		// 請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+		int totalSec = 256559;
+
+		int minute = 60; // 一分鐘的秒數
+		int hour = minute * 60; // 一小時的秒數
+		int day = hour * 24; // 一天的秒數
+
+		System.out.println("\n第三題");
+		System.out.printf("%d秒為%d天、%d小時、%d分與%d秒%n", totalSec, totalSec / day, totalSec % day / hour,
+				totalSec % hour / minute, totalSec % minute);
+
+		
+		// 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積與圓周長
+		final double PI = 3.1415;
+		double radius = 5;
+		double area = PI * Math.pow(radius, 2);
+		double circumference = 2 * PI * radius;
+
+		System.out.println("\n第四題");
+		System.out.printf("圓面積=%.2f", area);
+		System.out.printf("%n圓周長=%.2f", circumference);
+
+		
+		// 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,
+		// 請用程式計算10年後,本金加利息共有多少錢?
+		// 複利計算公式：本利和 = 本金 × (1 + 年利率)^期間
+		int principal = 150_0000;
+		double rate = 0.02f;
+		int period = 10;
+		double amount = principal * Math.pow((1 + rate), period);
+
+		System.out.println("\n\n第五題");
+		System.out.printf("本利和=%.2f元", amount);
+
+		
+		// 請說明以下三個運算式輸出結果的邏輯
+		System.out.println("\n\n第六題");
+		System.out.println(5 + 5); // 兩個5都為整數，輸出結果為整數相加，因此會等於10。
+		System.out.println(5 + '5'); // 第二個5為字元，根據Unicode編碼表5為0x35(16進位)，轉換為10進為等於53(3*16+5)，因此輸出結果會等於5。
+		System.out.println(5 + "5"); // 第二個5為字串，輸出結果為數字和字串串接，因此會等於55。
+
+	}
+}
---
+++ src/hw/Hw2.java
@@ -0,0 +1,152 @@
+package hw;
+
+public class Hw2 {
+	public static void main(String[] args) {
+
+		// 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
+		int sum = 0;
+		for (int i=2; i<=1000; i=i+2) {
+			sum += i;
+		}
+		System.out.println("第一題");
+		System.out.println("1~1000的偶數和 = " + sum);
+
+		
+		
+		// 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
+		int product1 = 1;
+		for (int i=1; i<=10; i++) {
+			product1 *= i;
+		}
+		System.out.println("\n第二題");
+		System.out.println("1~10的連乘積 = " + product1);
+
+		
+		
+		// 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
+		int product2 = 1;
+		int n = 1;
+		while (n <= 10) {
+			product2 *= n;
+			n++;
+		}
+		System.out.println("\n第三題");
+		System.out.println("1~10的連乘積 = " + product2);
+		
+		
+		
+		// 請設計一隻Java程式,輸出結果為以下:
+		// 1 4 9 16 25 36 49 64 81 100
+		System.out.println("\n第四題");
+
+		for (int i=1; i<=10; i++) {
+			int square = (int) Math.pow(i, 2);
+			System.out.print(square + " ");
+		}
+		
+		
+		
+		// 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。
+		// 請設計一隻程式,輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
+		System.out.println("\n\n第五題");
+		int count = 0;
+		for(int i=1; i<=49; i++) {
+			if(i%10 == 4 || i > 39) {
+				continue;
+			}
+			System.out.print(i+ "\t");
+			
+			count++;
+			if(count%9 == 0) {
+				System.out.println();
+			}
+		}
+		
+		
+		
+		// 請設計一隻Java程式,輸出結果為以下:
+		/*
+		   1 2 3 4 5 6 7 8 9 10 
+		   1 2 3 4 5 6 7 8 9 
+		   1 2 3 4 5 6 7 8 
+		   1 2 3 4 5 6 7 
+		   1 2 3 4 5 6 
+		   1 2 3 4 5
+		   1 2 3 4
+		   1 2 3
+		   1 2
+		   1  
+		 */
+		System.out.println("\n\n第六題");
+		
+		for(int i=10; i>=1; i--) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(j+ " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		// 請設計一隻Java程式,輸出結果為以下:
+		/*
+			A
+			BB
+			CCC
+			DDDD
+			EEEEE
+			FFFFFF
+		 */
+		System.out.println("\n第七題");
+		
+		for(int i=1; i<=6; i++) {
+			for(int j=1; j<=i; j++) {
+				char ch = 64;         // A的char為65
+				ch += i;
+				System.out.print(ch+ " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		// 課堂練習:使用不同的迴圈方法，輸出九九乘法表
+		// 使用 for + while
+		System.out.println("\n課堂練習:");
+		System.out.println("使用 for + while 輸出九九乘法表");
+		for(int i=1; i<=9; i++) {
+			int j=1;
+			while(j<=9) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+			}
+			System.out.println();
+		}
+		
+		// 使用 for + do while
+		System.out.println("\n使用 for + do while 輸出九九乘法表");
+		for(int i=1; i<=9; i++) {
+			int j=1;
+			do {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+			}while(j<=9);
+			System.out.println();
+		}
+		
+		// 使用 while + do while
+		System.out.println("\n使用 while + do while 輸出九九乘法表");
+		int i=1;
+		while(i<=9) {		
+			int j=1;
+			do {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+				j++;
+			}while(j<=9);
+			i++;
+			System.out.println();
+		}
+		
+		
+	}
+}
